15c0c7e9b8e5689f0f58064551c1fd34ed112306
D G <dmitrigatto@gmail.com.com>
2025-10-15 04:48:32 -0400

    updated changes

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,7 +1,53 @@
+# require 'yaml'
+
+# class Monster
+#     attr_accessor :name, :hp, :min_damage, :max_damage, :image_path, :equipment
+
+#     def initialize(name:, hp:, min_damage:, max_damage:, image_path:)
+#         @name = name
+#         @hp = hp
+#         @min_damage = min_damage
+#         @max_damage = max_damage
+#         @image_path = image_path
+#         @equipment = []
+#     end
+
+#     def alive?
+#         @hp     > 0
+#     end
+
+#     def attack(player)
+#         damage = rand(@min_damage..@max_damage)
+#         player.hp -= damage
+#         "#{@name} attacks #{player.name} for #{damage} damage!"
+#     end
+
+#     # factory method to load from YAML
+#     def self.load_monsters(file = "monsters.yml")
+#         full_path = File.expand_path("../data/#{file}", __dir__)
+
+#         puts full_path
+#         data = YAML.load_file(full_path)
+#         return [] unless data.is_a?(Array)
+
+#         data.map do |m|
+#             Monster.new(
+#             name: m["name"],
+#             hp: m["hp"],
+#             min_damage: m["min_damage"],
+#             max_damage: m["max_damage"],
+#             image_path: m["image"]
+#             )
+#         end
+#     end
+
+# end
+
+
 require 'yaml'
 
 class Monster
-    attr_accessor :name, :hp, :min_damage, :max_damage, :image_path, :equipment
+    attr_accessor :name, :hp, :min_damage, :max_damage, :image_path
 
     def initialize(name:, hp:, min_damage:, max_damage:, image_path:)
         @name = name
@@ -9,36 +55,26 @@ class Monster
         @min_damage = min_damage
         @max_damage = max_damage
         @image_path = image_path
-        @equipment = []
     end
 
     def alive?
-        @hp     > 0
+        @hp > 0
     end
 
-    def attack(player)
-        damage = rand(@min_damage..@max_damage)
-        player.hp -= damage
-        "#{@name} attacks #{player.name} for #{damage} damage!"
-    end
-
-    # factory method to load from YAML
-    def self.load_monsters(file = "monsters.yml")
+    def self.load_monsters
+        file = "monsters.yml"
         full_path = File.expand_path("../data/#{file}", __dir__)
-
-        puts full_path
         data = YAML.load_file(full_path)
-        return [] unless data.is_a?(Array)
-
         data.map do |m|
-            Monster.new(
+        puts "[DEBUG] Monsters loaded: #{@monsters.inspect}"
+
+        Monster.new(
             name: m["name"],
             hp: m["hp"],
             min_damage: m["min_damage"],
             max_damage: m["max_damage"],
-            image_path: m["image"]
-            )
+            image_path: m["image_path"]
+        )
         end
     end
-
 end
--- main.rb
+++ main.rb
@@ -1,40 +1,8 @@
-require 'ruby2d'
-require_relative '../lib/player'
-require_relative '../lib/monster'
-require_relative '../lib/battle'
+require_relative 'ui/game_window'
+require_relative 'lib/monster'
+require_relative 'lib/player'
 
-class GameWindow
-    def initialize(player, monsters)
-        @player = player
-        @monsters = monsters
-        @monster_image = nil
-    end
+player = Player.new(name: "Xuin", hp: 12)
+monsters = Monster.load_monsters
 
-    def start
-        set title: "Adventure!", width: 800, height: 600, resizable: true
-        show_menu
-        show
-    end
-
-    def show_menu
-        puts "Choose a monster:"
-        @monsters.each_with_index do |m, i|
-        puts "#{i + 1}. #{m.name}"
-        end
-        print "> "
-        choice = gets.to_i - 1
-        start_battle(@monsters[choice])
-    end
-
-    def start_battle(monster)
-        @monster_image = Image.new(monster.image_path)
-        center_image(@monster_image)
-    end
-
-    private
-
-    def center_image(image)
-        image.x = Window.width / 2 - image.width / 2
-        image.y = Window.height / 2 - image.height / 2
-    end
-end
+GameWindow.new(player, monsters).start
